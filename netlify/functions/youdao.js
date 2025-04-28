d71fb3da81f2890e2de5e770c9541b7516ac33f7
penny0531 <pennymoon0531@gmail.com>
2025-04-28 09:54:03 +0800

    fix: update youdao.js with basic function

--- netlify/functions/youdao.js
+++ netlify/functions/youdao.js
@@ -1,37 +1,6 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-const crypto = require('crypto');
-
-exports.handler = async function(event, context) {
-  const q = event.queryStringParameters.q;
-
-  const appKey = '4733c68e76321b78';       // ✍ 这里填你的
-  const appSecret = 'vEEVfY9i86WwYm35kqIqedzZOcaZnicp'; // ✍ 这里填你的
-
-  const salt = Date.now();
-  const curtime = Math.floor(Date.now() / 1000);
-  const str1 = appKey + q + salt + curtime + appSecret;
-  const sign = crypto.createHash('sha256').update(str1).digest('hex');
-
-  const formData = new URLSearchParams();
-  formData.append('q', q);
-  formData.append('from', 'en');
-  formData.append('to', 'zh-CHS');
-  formData.append('appKey', appKey);
-  formData.append('salt', salt);
-  formData.append('sign', sign);
-  formData.append('signType', 'v3');
-  formData.append('curtime', curtime);
-
-  const response = await fetch('https://openapi.youdao.com/api', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: formData
-  });
-
-  const data = await response.json();
-
+exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: JSON.stringify({ message: "Youdao function is working!" })
   };
 };
