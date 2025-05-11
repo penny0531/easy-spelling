9e75f7527d53203954872b50d970b796fd38a088
penny0531 <pennymoon0531@gmail.com>
2025-05-11 20:01:33 +0800

    feat: 极简模式，所有人直接进入练习，管理页面仅老师专用

--- netlify/functions/validateCode.js
+++ netlify/functions/validateCode.js
@@ -1,16 +1,7 @@
 exports.handler = async function(event) {
-  const { code } = JSON.parse(event.body);
-  if (code && code.toUpperCase() === 'ADMIN') {
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ type: 'admin', expiry: null })
-    };
-  }
-  // 其他情况都当普通用户
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ type: 'user', expiry: null })
   };
-} 
+}
